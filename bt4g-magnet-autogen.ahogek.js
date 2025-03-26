380e4d95251c9fd1a019304938f69cafd7aefdf7
AhogeK <ahogek@gmail.com>
2025-03-26 16:02:48 +0800

    test: update webhook test

--- bt4g-magnet-autogen.ahogek.js
+++ bt4g-magnet-autogen.ahogek.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BT4G Magnet AutoGen
 // @namespace    https://ahogek.com
-// @version      1.0.2
+// @version      1.0.3
 // @description  自动转换BT4G哈希到磁力链接并延迟尝试关闭页面
 // @author       AhogeK
 // @match        *://*.bt4g.org/*
